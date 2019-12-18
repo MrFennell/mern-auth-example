ab3bb1c6b5f6fce1131f6611705818faa2f7e7e4
Ryan <rfennell001@gmail.com>
2019-12-18 16:41:58 -0500

    Added react, redux and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+              <Landing/>
+          </Route>
+          <Route exact path="/register">
+              <Register />
+          </Route>
+          <Route exact path="/login">
+              <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
   );
 }
 
