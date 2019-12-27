6b39c698e101a9e7d360d1ddd8e4e06122367a9d
Ryan <rfennell001@gmail.com>
2019-12-27 16:19:03 -0500

    Auth now working, updated router logic to V4 compatible

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,60 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import './App.css'; 
+import { Router, Switch, Route} from "react-router-dom";
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
-import { Provider } from "react-redux";
-import store from "./store";
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import PrivateRoute from './components/private-route/PrivateRoute';
+import Dashboard from './components/dashboard/Dashboard';
+
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <div className="App">
         <Navbar />
-        <Switch>
-          <Route exact path="/">
+        <Switch >
+          <Route  exact path="/">
               <Landing/>
           </Route>
           <Route exact path="/register">
               <Register />
           </Route>
           <Route exact path="/login">
-              <Login />
+              <Login history={history} />
           </Route>
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
         </Switch>
       </div>
     </Router>
